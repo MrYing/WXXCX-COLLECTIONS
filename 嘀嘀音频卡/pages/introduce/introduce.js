82ffd01094965e215bb0b9171c37cc257265e0e1
xianSkyKing <xianSky@outlook.com>
2017-02-16 16:35:25 +0800

    音频卡产品简介

---
+++ 嘀嘀音频卡/pages/introduce/introduce.js
@@ -0,0 +1,38 @@
+// pages/introduce/introduce.js
+Page({
+  data:{
+    introducelist:[
+      {
+        "title": "滴滴银行卡",
+        "imgurl": "/images/product_introduce/i_1.jpg"
+      },
+      {
+        "title": "滴安全：放心支付",
+        "imgurl": "/images/product_introduce/i_2.jpg"
+      },
+      {
+        "title": "滴支付：便捷支付",
+        "imgurl": "/images/product_introduce/i_3.jpg"
+      },
+      {
+        "title": "滴实惠：线上线下快速支付，享受优惠",
+        "imgurl": "/images/product_introduce/i_4.jpg"
+      }
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
