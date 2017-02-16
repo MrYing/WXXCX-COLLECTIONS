0acf9b117303c41ac6b17783a1dcbec60b877b20
xianSkyKing <xianSky@outlook.com>
2017-02-16 16:27:15 +0800

    关于我们公司信息

---
+++ 嘀嘀音频卡/pages/aboutus/aboutus.js
@@ -0,0 +1,26 @@
+// pages/aboutus/aboutus.js
+Page({
+  data: {
+    latitude: 23.11197,
+    longitude: 113.40238,
+    markers: [{
+      latitude: 23.11197,
+      longitude:113.40238,
+      name: '广州市天河区东圃二马路67号大埔商贸园8号楼225,228室'
+    }]
+},onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
